43e36fed1181444c90903253e3da030dfd3e4b1c
efren sandoval <efren.sandoval@epn.edu.ec>
2019-04-15 14:02:49 -0500

    Depues de aplicar property binding

--- 01-Angular/galeria/src/app/item-galeria/item-galeria.component.ts
+++ 01-Angular/galeria/src/app/item-galeria/item-galeria.component.ts
@@ -15,6 +15,8 @@ export class ItemGaleriaComponent implements OnInit {
   @Input()
   textoBoton;
 
+  urlImagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Madre_e_hija_disfrutando_de_la_playa_de_Newport%2C_California.JPG/1200px-Madre_e_hija_disfrutando_de_la_playa_de_Newport%2C_California.JPG";
+
   alertar() {
     alert('Pasando mensajes al padre desde ' + this.nombreItem);
   }
