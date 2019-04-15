70255b4273f6d0d36ba2a205d6d6023ff6cfca76
efren sandoval <efren.sandoval@epn.edu.ec>
2019-04-15 14:43:28 -0500

    Cambio de imagen luego de clic sobre imagen

--- 01-Angular/galeria/src/app/item-galeria/item-galeria.component.ts
+++ 01-Angular/galeria/src/app/item-galeria/item-galeria.component.ts
@@ -15,10 +15,22 @@ export class ItemGaleriaComponent implements OnInit {
   @Input()
   textoBoton;
 
-  urlImagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Madre_e_hija_disfrutando_de_la_playa_de_Newport%2C_California.JPG/1200px-Madre_e_hija_disfrutando_de_la_playa_de_Newport%2C_California.JPG";
+  urlImagen = 'https://www.demos.org/sites/default/files/blog/glacier-national-park-montana-223702.jpg';
 
   alertar() {
     alert('Pasando mensajes al padre desde ' + this.nombreItem);
+  }
+
+  cambiarImagen() {
+    const montania = 'https://www.demos.org/sites/default/files/blog/glacier-national-park-montana-223702.jpg';
+      // tslint:disable-next-line:max-line-length
+    const playa = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Madre_e_hija_disfrutando_de_la_playa_de_Newport%2C_California.JPG/1200px-Madre_e_hija_disfrutando_de_la_playa_de_Newport%2C_California.JPG';
+
+    if (this.urlImagen === montania) {
+      this.urlImagen = playa;
+    } else {
+      this.urlImagen = montania;
+    }
   }
 
   constructor() { }
