fb4cbc665e6a029cf045f42082e9c43992f1cf7e
efren sandoval <efren.sandoval@epn.edu.ec>
2019-04-22 18:02:54 -0500

    Carrito a medias

--- 01-Angular/galeria/src/app/servicios/carrito/carrito.service.ts
+++ 01-Angular/galeria/src/app/servicios/carrito/carrito.service.ts
@@ -1,6 +1,29 @@
 import { Injectable } from '@angular/core';
+import { ItemCarritoComprasInterface, ItemCarritoCompras } from 'src/interfaces/item-carrito-compras';
 
 @Injectable()
 export class CarritoService {
-carritoCompras = [];
+carritoCompras: ItemCarritoCompras[] = [];
+
+agregarCarritoDeCompras(itemCarrito: ItemCarritoCompras): ItemCarritoCompras[] {
+    const identificador = itemCarrito.valor;
+    const existeElItem = this.carritoCompras.some(
+        (item: ItemCarritoCompras, indice) => {
+            if (item.valor === identificador) {
+                return 
+            }
+            return item.valor === identificador;
+        }
+    );
+}
+
+agregarCarrito(valorCarrito: number) {
+    const itemCarrito = {
+      valor: valorCarrito,
+      nombreTienda: this.titulo
+    };
+
+    this._carritoService.carritoCompras.splice(0, 0, itemCarrito);
+  }
+
 }
