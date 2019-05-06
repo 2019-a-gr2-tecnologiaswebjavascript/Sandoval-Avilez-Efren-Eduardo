bb688ed77380144015b11a4f7b0a114962fe39f8
efren sandoval <efren.sandoval@epn.edu.ec>
2019-05-06 11:39:31 -0500

    tarea primeros dos componentes

---
+++ tareas-Angular/menu-tablita-tartaro/src/app/barra-opciones/barra-opciones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-barra-opciones',
+  templateUrl: './barra-opciones.component.html',
+  styleUrls: ['./barra-opciones.component.css']
+})
+export class BarraOpcionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
