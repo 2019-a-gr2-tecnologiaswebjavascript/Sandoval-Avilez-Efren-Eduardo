4048d8d7f77101dd2232dfe41bbe3bf9ac4d0c5c
efren sandoval <efren.sandoval@epn.edu.ec>
2019-06-02 15:19:11 -0500

    Registro cajero module y componente parcialmente modificado

---
+++ 01-Examen/examen-primer-parcial/src/app/registro-cajero/registro-cajero.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegistroCajeroComponent } from './registro-cajero.component';
+
+@NgModule({
+  declarations: [RegistroCajeroComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class RegistroCajeroModule { }
