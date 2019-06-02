a2bc63f9b1af36591baa13f394b5f5b507334ec6
efren sandoval <efren.sandoval@epn.edu.ec>
2019-06-02 15:21:55 -0500

    tabla-tiendas component creado

---
+++ 01-Examen/examen-primer-parcial/src/app/gestionar-tiendas/tabla-tiendas/tabla-tiendas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-tiendas',
+  templateUrl: './tabla-tiendas.component.html',
+  styleUrls: ['./tabla-tiendas.component.css']
+})
+export class TablaTiendasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
