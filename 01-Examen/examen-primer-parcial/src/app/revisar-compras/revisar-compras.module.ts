faa62613bdd83ffb0615e9d32aa120f7b87f059e
efren sandoval <efren.sandoval@epn.edu.ec>
2019-06-03 00:33:27 -0500

    Componente padre revisar-compras

---
+++ 01-Examen/examen-primer-parcial/src/app/revisar-compras/revisar-compras.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RevisarComprasComponent } from './revisar-compras.component';
+
+@NgModule({
+  declarations: [RevisarComprasComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class RevisarComprasModule { }
