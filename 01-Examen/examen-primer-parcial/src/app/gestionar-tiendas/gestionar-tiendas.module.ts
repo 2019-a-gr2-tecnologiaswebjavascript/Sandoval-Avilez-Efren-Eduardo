74e06db57a0c0c0e628f3c04c717f8298166dfb0
efren sandoval <efren.sandoval@epn.edu.ec>
2019-06-03 00:32:54 -0500

    Componente padre gestionar-tiendas

--- 01-Examen/examen-primer-parcial/src/app/gestionar-tiendas/gestionar-tiendas.module.ts
+++ 01-Examen/examen-primer-parcial/src/app/gestionar-tiendas/gestionar-tiendas.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BarraBusquedaTiendasComponent } from './barra-busqueda-tiendas/barra-busqueda-tiendas.component';
 import { TablaTiendasComponent } from './tabla-tiendas/tabla-tiendas.component';
+import { GestionarTiendasComponent } from './gestionar-tiendas.component';
 
 @NgModule({
-  declarations: [BarraBusquedaTiendasComponent, TablaTiendasComponent],
+  declarations: [BarraBusquedaTiendasComponent, TablaTiendasComponent, GestionarTiendasComponent],
   imports: [
     CommonModule
   ]
