be50ad18546ae2d7d944ba7af0ed81604adf8931
efren sandoval <efren.sandoval@epn.edu.ec>
2019-06-03 00:31:52 -0500

    Componente padre comprar

---
+++ 01-Examen/examen-primer-parcial/src/app/comprar/comprar.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ComprarComponent } from './comprar.component';
+
+@NgModule({
+  declarations: [ComprarComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComprarModule { }
