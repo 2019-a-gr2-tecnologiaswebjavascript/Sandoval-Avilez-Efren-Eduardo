46bfcffdfb06174b50553657fbda1394f455f9e6
efren sandoval <efren.sandoval@epn.edu.ec>
2019-06-03 00:39:34 -0500

    Modificadas todas las rutas

--- 01-Examen/examen-primer-parcial/src/app/app-routing.module.ts
+++ 01-Examen/examen-primer-parcial/src/app/app-routing.module.ts
@@ -1,30 +1,49 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginCajeroComponent } from './login-cajero/login-cajero.component';
+import { RegistroCajeroComponent } from './registro-cajero/registro-cajero.component';
+import { MenuPrincipalComponent } from './menu-principal/menu-principal.component';
+import { RevisarComprasComponent } from './revisar-compras/revisar-compras.component';
+import { GestionarTiendasComponent } from './gestionar-tiendas/gestionar-tiendas.component';
+import { ComprarComponent } from './comprar/comprar.component';
+import { RutaNoEncontradaComponent } from './ruta-no-encontrada/ruta-no-encontrada.component';
+import { EstaLogueadoGuard } from './servicios/guards/esta-logueado.guard';
 
 const routes: Routes = [
   {
-    path: 'menuPrincipal',
-    component: MenuPrincipalComponent
+    path: 'app/login',
+    component: LoginCajeroComponent
+  },
+  {
+    path: 'registrar-cajero',
+    component: RegistroCajeroComponent
+  },
+  {
+    path: 'menu-principal',
+    component: MenuPrincipalComponent,
+    canActivate: [
+      EstaLogueadoGuard
+    ]
   },
   {
     path: 'listar-compras',
-    component: RutaCreditosComponent,
-    canActivate:[
-     EstaLogeadoService
+    component: RevisarComprasComponent,
+    canActivate: [
+      EstaLogueadoGuard
     ]
   },
   {
     path: 'gestion-tiendas',
-    component: RutaCreditosComponent,
-    canActivate:[
-      EstaLogeadoService
+    component: GestionarTiendasComponent,
+    canActivate: [
+      EstaLogueadoGuard
     ]
   },
   {
     path: 'comprar',
-    component: RutaCreditosComponent,
-    canActivate:[
-      EstaLogeadoService
+    component: ComprarComponent,
+    canActivate: [
+      EstaLogueadoGuard
     ]
   },
   {
@@ -33,11 +52,11 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: '/login',
+    redirectTo: '/app/login',
     pathMatch: 'full'
   },
   {
-   path: '**', 
+   path: '**',
    component: RutaNoEncontradaComponent
   }
 ];
