56734ea885f56943ac02a9c3574b2b4adae4f041
efren sandoval <efren.sandoval@epn.edu.ec>
2019-06-03 00:34:13 -0500

    Componente padre ruta-no-encontrada y mofulo del mismo nombre

---
+++ 01-Examen/examen-primer-parcial/src/app/ruta-no-encontrada/ruta-no-encontrada.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RutaNoEncontradaComponent } from './ruta-no-encontrada.component';
+
+@NgModule({
+  declarations: [RutaNoEncontradaComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class RutaNoEncontradaModule { }
