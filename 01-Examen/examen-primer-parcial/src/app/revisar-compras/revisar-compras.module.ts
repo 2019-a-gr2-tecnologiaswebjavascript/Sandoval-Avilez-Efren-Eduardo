f05db968af3192aeae947ed0febbccf441be8582
efren sandoval <efren.sandoval@epn.edu.ec>
2019-06-04 08:04:49 -0500

    Modulo y componente principal revisar-compras listo

--- 01-Examen/examen-primer-parcial/src/app/revisar-compras/revisar-compras.module.ts
+++ 01-Examen/examen-primer-parcial/src/app/revisar-compras/revisar-compras.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RevisarComprasComponent } from './revisar-compras.component';
+import { ComponentesComunesModule } from '../componentes-comunes/componentes-comunes.module';
 
 @NgModule({
-  declarations: [RevisarComprasComponent],
+  declarations: [
+    RevisarComprasComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentesComunesModule
   ]
 })
 export class RevisarComprasModule { }
