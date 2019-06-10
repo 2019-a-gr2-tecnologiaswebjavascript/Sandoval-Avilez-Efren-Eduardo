1994d90cf08a735678fd271c2abbe69eea0e1778
efren sandoval <efren.sandoval@epn.edu.ec>
2019-06-10 15:45:32 -0500

    Commit inicial Sails

---
+++ 05-backend/tienda-backend/config/session.js
@@ -0,0 +1,39 @@
+/**
+ * Session Configuration
+ * (sails.config.session)
+ *
+ * Use the settings below to configure session integration in your app.
+ * (for additional recommended settings, see `config/env/production.js`)
+ *
+ * For all available options, see:
+ * https://sailsjs.com/config/session
+ */
+
+module.exports.session = {
+
+  /***************************************************************************
+  *                                                                          *
+  * Session secret is automatically generated when your new app is created   *
+  * Replace at your own risk in production-- you will invalidate the cookies *
+  * of your users, forcing them to log in again.                             *
+  *                                                                          *
+  ***************************************************************************/
+  secret: '7709a902d0b45de7a2c2798a68a1f43b',
+
+
+  /***************************************************************************
+  *                                                                          *
+  * Customize when built-in session support will be skipped.                 *
+  *                                                                          *
+  * (Useful for performance tuning; particularly to avoid wasting cycles on  *
+  * session management when responding to simple requests for static assets, *
+  * like images or stylesheets.)                                             *
+  *                                                                          *
+  * https://sailsjs.com/config/session                                       *
+  *                                                                          *
+  ***************************************************************************/
+  // isSessionDisabled: function (req){
+  //   return !!req.path.match(req._sails.LOOKS_LIKE_ASSET_RX);
+  // },
+
+};
