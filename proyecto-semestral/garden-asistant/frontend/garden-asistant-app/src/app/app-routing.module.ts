6e434a1a1a1377df9c98935770464b275d89297a
efren sandoval <efren.sandoval@epn.edu.ec>
2019-07-26 17:09:52 -0500

    Las rutas catalogo y controles ya definidas.

--- proyecto-semestral/garden-asistant/frontend/garden-asistant-app/src/app/app-routing.module.ts
+++ proyecto-semestral/garden-asistant/frontend/garden-asistant-app/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login/login.component';
+import { PresentacionComponent } from './presentacion/presentacion/presentacion.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'catalogo', component: PresentacionComponent},
+  { path: 'controles', component: DashboardComponent }
 ];
 
 @NgModule({
