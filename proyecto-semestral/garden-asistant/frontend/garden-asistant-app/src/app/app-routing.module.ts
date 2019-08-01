2e14f0b6640b1ac2aec2937c07e45601bc311bb6
efren sandoval <efren.sandoval@epn.edu.ec>
2019-08-01 07:22:12 -0500

    Configurando rutas.

--- proyecto-semestral/garden-asistant/frontend/garden-asistant-app/src/app/app-routing.module.ts
+++ proyecto-semestral/garden-asistant/frontend/garden-asistant-app/src/app/app-routing.module.ts
@@ -3,13 +3,29 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login/login.component';
 import { PresentacionComponent } from './presentacion/presentacion/presentacion.component';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { EstaLogueadoGuard } from './servicios/guards/esta-logueado.guard';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'catalogo', component: PresentacionComponent},
-  { path: 'controles', component: DashboardComponent }
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'catalogo',
+    component: PresentacionComponent,
+    canActivate: [
+      EstaLogueadoGuard
+    ]
+  },
+  {
+    path: 'controles',
+    component: DashboardComponent,
+    canActivate: [
+      EstaLogueadoGuard
+    ]
+  }
 ];
 
 @NgModule({
