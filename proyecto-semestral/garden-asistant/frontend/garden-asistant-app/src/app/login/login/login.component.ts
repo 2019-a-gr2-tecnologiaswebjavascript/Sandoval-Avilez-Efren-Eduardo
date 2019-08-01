9351801547e992e4ba8445a7da2485867e68a425
efren sandoval <efren.sandoval@epn.edu.ec>
2019-08-01 07:20:07 -0500

    Impletación del mecanismo de login con backend.

--- proyecto-semestral/garden-asistant/frontend/garden-asistant-app/src/app/login/login/login.component.ts
+++ proyecto-semestral/garden-asistant/frontend/garden-asistant-app/src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/servicios/auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  nombreUsuario: string;
+  passwordUsuario: string;
 
-  constructor() { }
+  // tslint:disable-next-line:variable-name
+  constructor(private readonly _authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    console.log('Estamos logeando');
+    this._authService.login(this.nombreUsuario, this.passwordUsuario);
+  }
+  logout() {
+    console.log('Saliendo del sistema');
+    this._authService.logout();
   }
 
 }
